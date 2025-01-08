e56f26256379bd88718fb820d09fb944e200a67f
Fransi Mengesha <Francyayele@gmail.com>
2025-01-09 00:51:20 +0300

    new

--- components/ui/scroll-area.tsx
+++ components/ui/scroll-area.tsx
@@ -5,6 +5,7 @@ import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area";
 
 import { cn } from "@/lib/utils";
 
+// ScrollArea Component
 const ScrollArea = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
@@ -17,16 +18,33 @@ const ScrollArea = React.forwardRef<
     <ScrollAreaPrimitive.Viewport className="h-full w-full">
       {children}
     </ScrollAreaPrimitive.Viewport>
-    <ScrollAreaPrimitive.Scrollbar orientation="vertical">
-      <ScrollAreaPrimitive.Thumb className="bg-gray-400" />
-    </ScrollAreaPrimitive.Scrollbar>
-    <ScrollAreaPrimitive.Scrollbar orientation="horizontal">
-      <ScrollAreaPrimitive.Thumb className="bg-gray-400" />
-    </ScrollAreaPrimitive.Scrollbar>
-    <ScrollAreaPrimitive.Corner />
+    <ScrollBar orientation="vertical" />
+    <ScrollBar orientation="horizontal" />
   </ScrollAreaPrimitive.Root>
 ));
 
 ScrollArea.displayName = "ScrollArea";
 
-export default ScrollArea;
+// ScrollBar Component
+const ScrollBar = React.forwardRef<
+  React.ElementRef<typeof ScrollAreaPrimitive.Scrollbar>,
+  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Scrollbar>
+>(({ className, orientation = "vertical", ...props }, ref) => (
+  <ScrollAreaPrimitive.Scrollbar
+    ref={ref}
+    orientation={orientation}
+    className={cn(
+      "flex touch-none select-none bg-gray-200 p-0.5 transition-colors hover:bg-gray-300",
+      orientation === "vertical" ? "h-full w-2.5" : "h-2.5 w-full",
+      className
+    )}
+    {...props}
+  >
+    <ScrollAreaPrimitive.Thumb className="relative flex-1 bg-gray-500" />
+  </ScrollAreaPrimitive.Scrollbar>
+));
+
+ScrollBar.displayName = "ScrollBar";
+
+// Export components
+export { ScrollArea, ScrollBar };
